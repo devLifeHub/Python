1e6bbec802bf4e139b7a7e4431bc2ab17ba2f8ff
Aleksandr Saskevich <saskevich_aleksandr_tut_s@mail.ru>
2023-11-09 00:08:10 +0300

    fixed: simplifying word conversion through title()

---
+++ PyTasks/capital_letters/main.py
@@ -0,0 +1,9 @@
+# Вариант 1
+list_text = input('Введите строку: ').title()
+print('Результат:', list_text)
+
+# Вариант 2
+# list_text = input('Введите строку: ').lower().split(' ')
+# list_text = [word.capitalize() for word in list_text]
+# result = ' '.join(list_text)
+# print('Результат:', result)
