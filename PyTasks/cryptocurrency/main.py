dc836248cba47d665387db670bc8b0ed2f798f0e
Aleksandr Saskevich <saskevich_aleksandr_tut_s@mail.ru>
2023-11-09 00:12:28 +0300

    added: algorithms for working with dictionaries

---
+++ PyTasks/cryptocurrency/main.py
@@ -0,0 +1,65 @@
+data = {
+    "address": "0x544444444444",
+    "ETH": {
+        "balance": 444,
+        "total_in": 444,
+        "total_out": 4
+    },
+    "count_txs": 2,
+    "tokens": [
+        {
+            "fst_token_info": {
+                "address": "0x44444",
+                "name": "fdf",
+                "decimals": 0,
+                "symbol": "dsfdsf",
+                "total_supply": "3228562189",
+                "owner": "0x44444",
+                "last_updated": 1519022607901,
+                "issuances_count": 0,
+                "holders_count": 137528,
+                "price": False
+            },
+            "balance": 5000,
+            "totalIn": 0,
+            "total_out": 0
+        },
+        {
+            "sec_token_info": {
+                "address": "0x44444",
+                "name": "ggg",
+                "decimals": "2",
+                "symbol": "fff",
+                "total_supply": "250000000000",
+                "owner": "0x44444",
+                "last_updated": 1520452201,
+                "issuances_count": 0,
+                "holders_count": 20707,
+                "price": False
+            },
+            "balance": 500,
+            "totalIn": 0,
+            "total_out": 0
+        }
+    ]
+}
+
+
+# task 1
+keys = list(data.keys())
+print("Список ключей:", keys)
+
+values = list(data.values())
+print("Список значений:", values)
+
+# task 2
+data["ETH"]["total_diff"] = 100
+
+# task 3
+data["tokens"][0]["fst_token_info"]["name"] = "doge"
+
+# task 4
+data['ETH']['totalOut'] = data['tokens'][0].pop('total_out')
+
+# task 5
+data['tokens'][1]['sec_token_info']['total_price'] = data['tokens'][1]['sec_token_info'].pop('price')
